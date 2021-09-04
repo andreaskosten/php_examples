2fd17e8c3f9b699c590dfd38045872171f715ae5
Андрей Костенко <63039883+andreaskosten@users.noreply.github.com>
2021-09-04 08:34:20 +0300

    style improvement

--- useful_classes/Exiter.php
+++ useful_classes/Exiter.php
@@ -11,15 +11,14 @@ class Exiter
     public function __construct($status, $description, $array = [])
     {
         $finalArray = [];
-        if( !empty($array) ){
+        if (!empty($array)) {
             $finalArray = $array;
         }
 
-        if( $status == 0 || $status == 1 ){
+        if ($status == 0 || $status == 1) {
             $finalArray['status'] = $status;
             $finalArray['description'] = $description;
-        }
-        else{
+        } else {
             exit('ERROR, status is incorrect in __construct of Exiter');
         }
 
@@ -32,14 +31,9 @@ class Exiter
 $exitData['color_1'] = 'blue';
 $exitData['color_2'] = 'yellow';
 $exit = new Exiter(1, 'colors_extracted', $exitData);
-
-EXPECTING:
-{"color_1":"blue","color_2":"yellow","status":1,"description":"colors_extracted"}
-
+// >> {"color_1":"blue","color_2":"yellow","status":1,"description":"colors_extracted"}
 
 2. пример "плохого" выхода:
 $exit = new Exiter(0, 'database_error');
-
-EXPECTING:
-{"status":0,"description":"database_error"}
+// >> {"status":0,"description":"database_error"}
 */
